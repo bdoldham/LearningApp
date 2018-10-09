4d9d1e5ac114116b755badda9d3abed55a463220
bdoldham <bdoldham@gmail.com>
2018-10-09 08:19:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function helloWorld() {
+    var firstNumber = document.getElementById("firstNumber").value;
+    var secondNumber = document.getElementById("secondNumber").value;
+    alert(firstNumber);
+    alert("Hello World");
+}
+
