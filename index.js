68bfd15b3f1313250b3995373a6666cc1b61a697
bdoldham <bdoldham@gmail.com>
2020-11-15 08:31:57 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function helloWorld() {
-    var firstNumber = document.getElementById("firstNumber").value;
-    var secondNumber = document.getElementById("secondNumber").value;
-    alert(firstNumber);
-    alert("Hello World");
-}
-
